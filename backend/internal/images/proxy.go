50dc9229779256dcb371f4718dca248cb12b6a22
Hasibul Hasan <git.commit@hsblhsn.me>
2022-03-29 22:57:26 +0600

    refactor: do not return a proxy url if the image src is empty.

--- backend/internal/images/proxy.go
+++ backend/internal/images/proxy.go
@@ -6,5 +6,8 @@ import (
 )
 
 func ProxiedURL(src string, size ImageSize) string {
+	if src == "" {
+		return ""
+	}
 	return fmt.Sprintf("/api/v1/image.jpeg?src=%s&size=%s", url.QueryEscape(src), size)
 }
