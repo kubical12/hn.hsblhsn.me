766b802f99d82dc3c0e297c82c1779c480163594
Hasibul Hasan <git.commit@hsblhsn.me>
2022-04-15 17:16:56 +0600

    feat: add graphql explorer and playground.

--- backend/backend.go
+++ backend/backend.go
@@ -1,11 +1,22 @@
 package backend
 
 import (
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/gorilla/mux"
 	"github.com/hsblhsn/hn.hsblhsn.me/backend/graphql"
 )
 
 func RegisterRoutes(router *mux.Router, gql *graphql.GQLHandler, img *graphql.ImageHandler) {
-	router.Handle("/graphql", gql)
-	router.Handle("/images.jpeg", img)
+	router.Path("/graphql").
+		Methods(http.MethodGet, http.MethodPost).
+		Handler(gql)
+	router.Path("/explorer").
+		Methods(http.MethodGet).
+		Handler(playground.Handler("GraphQL Explorer", "/graphql"))
+	router.Path("/images.jpeg").
+		Methods(http.MethodGet).
+		Queries("size", "{size}", "src", "{src}").
+		Handler(img)
 }
