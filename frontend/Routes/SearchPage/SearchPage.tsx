a48d73a647b3c48673a1c5a39447a5695ec70d90
Hasibul Hasan <git.commit@hsblhsn.me>
2022-04-26 10:14:43 +0600

    refactor: search bar using enhancers.

--- frontend/Routes/SearchPage/SearchPage.tsx
+++ frontend/Routes/SearchPage/SearchPage.tsx
@@ -4,7 +4,7 @@ import { useSearchParams } from 'react-router-dom'
 import { LoadingScreen } from './LoadingScreen'
 import { ErrorScreen } from './ErrorScreen'
 import { FormEvent, Fragment, useCallback, useEffect, useState } from 'react'
-import { BaseInput } from 'baseui/input'
+import { Input } from 'baseui/input'
 import {
   ITEM_CARD_LIST_NODE_FIELDS,
   PaginatedItemCardList,
@@ -16,6 +16,7 @@ import { FormControl } from 'baseui/form-control'
 import AwesomeDebouncePromise from 'awesome-debounce-promise'
 import { HeadingXXLarge, ParagraphMedium } from 'baseui/typography'
 import { Head } from './Head'
+import { Search } from 'baseui/icon'
 
 const PAGE_INFO_FIELDS = gql`
   fragment PageInfoFields on PageInfo {
@@ -176,22 +177,16 @@ const SearchBar = ({ value, onChange }: SearchBarProps) => {
       <form onSubmit={focusOut} onReset={focusOut}>
         <FormControl
           //label="Search HackerNews"
-          caption="All search results are sorted by popularity."
+          caption="All search results are sorted by relevance."
         >
-          <BaseInput
+          <Input
             id="search-input"
             name="q"
             placeholder="Type to search..."
             value={value}
             onChange={(e) => onChange(e.currentTarget.value)}
             autoFocus={true}
-            overrides={{
-              InputContainer: {
-                style: ({ $theme }) => ({
-                  border: `2px solid  ${$theme.colors.borderOpaque}`,
-                }),
-              },
-            }}
+            startEnhancer={<Search size={24} />}
           />
         </FormControl>
       </form>
