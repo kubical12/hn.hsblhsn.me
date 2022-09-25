f0f09dcf12f93d784bc086eda4cef7ffda176aa9
Hasibul Hasan <git.commit@hsblhsn.me>
2022-09-25 23:06:47 +0200

    refactor(build): improve build files by using dynamic chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
+
 const proxyConfig = {
   '/graphql': {
     target: 'https://hn.hsblhsn.me/',
@@ -13,9 +16,26 @@ const proxyConfig = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: './frontend/build/stats.html',
+    }),
+    ViteMinifyPlugin({
+      esbuildTarget: 'es2015',
+    }),
+  ],
   build: {
     outDir: './frontend/build',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router', 'react-router-dom'],
+          baseui: ['baseui'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 700,
   },
   server: {
     proxy: proxyConfig,
